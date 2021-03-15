adfd4e4e11bd7c9a3b8864307df33c53b3ee9ad6
Imre Kaszab <imre.kaszab@supercharge.io>
2021-03-15 19:04:51 +0100

    feat: convert application class to kotlin

--- app/src/main/java/io/imrekaszab/algorithms/AlgorithmsApplication.java
+++
@@ -1,9 +0,0 @@
-package io.imrekaszab.algorithms;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class AlgorithmsApplication extends Application {
-}
