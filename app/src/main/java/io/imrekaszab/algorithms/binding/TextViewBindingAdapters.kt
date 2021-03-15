c4d9f81435a9cd160416f1d3d0a8e1a85835c0e5
Imre Kaszab <imre.kaszab@supercharge.io>
2021-03-15 19:01:03 +0100

    feat: convert binding adapters

---
+++ app/src/main/java/io/imrekaszab/algorithms/binding/TextViewBindingAdapters.kt
@@ -0,0 +1,26 @@
+package io.imrekaszab.algorithms.binding
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import io.imrekaszab.algorithms.R
+import io.imrekaszab.algorithms.model.Algorithm
+import io.imrekaszab.algorithms.model.AlgorithmType
+
+@BindingAdapter("title")
+fun TextView.bindTitleToTextView(model: Algorithm?) {
+    model ?: return
+    text = resources.getText(model.titleResId)
+}
+
+@BindingAdapter("sectionTitle")
+fun TextView.bindTitleToTextView(type: AlgorithmType?) {
+    type ?: return
+
+    setText(
+        if (type == AlgorithmType.SORT) {
+            R.string.algorithm_list_section_item_sort
+        } else {
+            R.string.algorithm_list_section_item_search
+        }
+    )
+}
