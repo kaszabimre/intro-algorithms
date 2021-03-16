6d5a1bbba1c5531fc570ccd02e70a5c2fa8b8bd5
Imre Kaszab <imre.kaszab@supercharge.io>
2021-03-16 17:46:34 +0100

    feat: add selection sort

--- app/src/main/java/io/imrekaszab/algorithms/data/model/SelectionSort.kt
+++ app/src/main/java/io/imrekaszab/algorithms/data/model/SelectionSort.kt
@@ -4,20 +4,23 @@ import androidx.annotation.StringRes
 import io.imrekaszab.algorithms.R
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class BubbleSort(array: IntArray) : SortAlgorithm(array) {
+class SelectionSort(array: IntArray) : SortAlgorithm(array) {
     @StringRes
-    override val titleResId = R.string.algorithm_list_item_bubble
+    override val titleResId = R.string.algorithm_list_item_selection
 
     override val performance = AlgorithmPerformance.QUADRATIC
 
     override suspend fun sort(outputFlow: MutableStateFlow<String>) {
+        printCurrentState(outputFlow)
         for (lastUnsortedIndex in intArray.size - 1 downTo 1) {
-            for (i in 0 until lastUnsortedIndex) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(i, i + 1)
-                    printCurrentState(outputFlow)
+            var largest = 0
+            for (i in 1..lastUnsortedIndex) {
+                if (intArray[i] >= intArray[largest]) {
+                    largest = i
                 }
             }
+            swap(largest, lastUnsortedIndex)
+            printCurrentState(outputFlow)
         }
     }
 }
--- app/src/main/java/io/imrekaszab/algorithms/data/service/MockAlgorithmFactory.kt
+++ app/src/main/java/io/imrekaszab/algorithms/data/service/MockAlgorithmFactory.kt
@@ -2,15 +2,19 @@ package io.imrekaszab.algorithms.data.service
 
 import io.bloco.faker.Faker
 import io.imrekaszab.algorithms.data.model.BubbleSort
+import io.imrekaszab.algorithms.data.model.SelectionSort
 import javax.inject.Inject
 
 class MockAlgorithmFactory @Inject constructor(private val faker: Faker) {
 
     fun getAlgorithmList() = arrayListOf(
-        bubbleSort()
+        bubbleSort(),
+        selectionSort()
     )
 
     private fun bubbleSort() = BubbleSort(getIntArray())
+
+    private fun selectionSort() = SelectionSort(getIntArray())
 
     private fun getIntArray() = IntRange(1, 10)
         .map { faker.number.between(-20, 20) }.toIntArray()
