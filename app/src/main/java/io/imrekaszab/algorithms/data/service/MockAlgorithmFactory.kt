5068bc46936c10084bad029ca24a3e0c91a12bca
Imre Kaszab <imre.kaszab@supercharge.io>
2021-03-16 17:56:28 +0100

    feat: add merge sort

--- app/src/main/java/io/imrekaszab/algorithms/data/service/MockAlgorithmFactory.kt
+++ app/src/main/java/io/imrekaszab/algorithms/data/service/MockAlgorithmFactory.kt
@@ -2,6 +2,7 @@ package io.imrekaszab.algorithms.data.service
 
 import io.bloco.faker.Faker
 import io.imrekaszab.algorithms.data.model.BubbleSort
+import io.imrekaszab.algorithms.data.model.MergeSort
 import io.imrekaszab.algorithms.data.model.SelectionSort
 import javax.inject.Inject
 
@@ -9,12 +10,13 @@ class MockAlgorithmFactory @Inject constructor(private val faker: Faker) {
 
     fun getAlgorithmList() = arrayListOf(
         bubbleSort(),
-        selectionSort()
+        selectionSort(),
+        mergeSort()
     )
 
     private fun bubbleSort() = BubbleSort(getIntArray())
-
     private fun selectionSort() = SelectionSort(getIntArray())
+    private fun mergeSort() = MergeSort(getIntArray())
 
     private fun getIntArray() = IntRange(1, 10)
         .map { faker.number.between(-20, 20) }.toIntArray()
